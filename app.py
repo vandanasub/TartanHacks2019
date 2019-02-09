b7020f918f38b21423ad03ddb3205507aa5e2037
IshaniSanturkar <ishani.santurkar@gmail.com>
2019-02-09 10:53:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, render_template, request, Markup, flash
+from flask_bootstrap import Bootstrap
 from analyzeImage import run
+import subprocess
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = os.urandom(24)
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,10 +19,13 @@ def upload_file():
 	file = request.files['image']
 	f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
 	file.save(f)
-	res = run(f)
-	print(res+"\n")
+	(res, keyPhr) = run(f)
 	res = Markup("<p>"+res+"</p>")
-	flash(res)
+	res1 = subprocess.run(["python","test.py", "foo bar"])
+	print(res1.stdout)
+	flash(res,'message')
+	for word in keyPhr:
+		flash(word, 'keyPhrase')
 	return render_template('home.html')
 
 if __name__ == "__main__":
