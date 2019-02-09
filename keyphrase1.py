e1f980102184e7ce32e7c2763d1c8d8abbd4ceaa
IshaniSanturkar <ishani.santurkar@gmail.com>
2019-02-08 23:34:35 -0500

    Update keyphrase1.py

--- keyphrase1.py
+++ keyphrase1.py
@@ -2,18 +2,18 @@ import pprint
 subscription_key = "8e4496657d114d88a93dc38582c339dc"
 assert subscription_key
 import requests
-text_analytics_base_url = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/"
-key_phrase_api_url = text_analytics_base_url + "keyPhrases"
-print(key_phrase_api_url)
-f = open("text.txt", "r")
-thetext = ""
-for line in f:
-	thetext += line.rstrip()
+import json
 
-documents = {'documents' : [
-  {'id': '1', 'language': 'en', 'text': thetext},
-]}
-headers   = {'Ocp-Apim-Subscription-Key': subscription_key}
-response  = requests.post(key_phrase_api_url, headers=headers, json=documents)
-key_phrases = response.json()
-pprint.pprint(key_phrases)
+def keyPhrase(text):
+	text_analytics_base_url = "https://eastus.api.cognitive.microsoft.com/text/analytics/v2.0/"
+	key_phrase_api_url = text_analytics_base_url + "keyPhrases"
+	
+	thetext = text
+
+	documents = {'documents' : [
+	  {'id': '1', 'language': 'en', 'text': thetext},
+	]}
+	headers   = {'Ocp-Apim-Subscription-Key': subscription_key}
+	response  = requests.post(key_phrase_api_url, headers=headers, json=documents)
+	key_phrases = response.json()["documents"][0]['keyPhrases']
+	return key_phrases
